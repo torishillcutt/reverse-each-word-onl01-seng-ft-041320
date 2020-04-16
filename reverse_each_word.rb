fd49d2e5bf5a74351b905bc823fe1408da834fb0
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-16 19:45:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   string_into_array = string.split
-  string_into_array.map { |word| word.reverse  }
-  string_into_array
+  return_array << string_into_array.map { |word| word.reverse  }
+
 end
